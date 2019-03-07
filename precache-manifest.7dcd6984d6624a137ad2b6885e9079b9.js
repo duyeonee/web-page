2cedc775d40a8149305a131fe9690075b4212b72
duyeon kim <duyeon@kse.kaist.ac.kr>
2019-03-07 18:04:41 +0900

    Updates

---
+++ precache-manifest.7dcd6984d6624a137ad2b6885e9079b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9622e0642d479f7037f5",
+    "url": "/web-page/static/js/runtime~main.44bbcfec.js"
+  },
+  {
+    "revision": "207f0737eab36e4dfbb2",
+    "url": "/web-page/static/js/main.a8777dfb.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/web-page/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "207f0737eab36e4dfbb2",
+    "url": "/web-page/static/css/main.50314d3d.chunk.css"
+  },
+  {
+    "revision": "7b0877b6cf30dd0a1ae887a4bb0eb200",
+    "url": "/web-page/index.html"
+  }
+];
